410889c59c66afdcd944929b67fd27ef2f4fa3d0
chertkovMV <mishaschertkov@gmail.com>
2024-04-08 20:01:07 +0400

    рекурсивно вычисляет n-ое число Фибоначчи

--- Hello World/Hello World/main.cpp
+++ Hello World/Hello World/main.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
 using namespace std;
+
+int fib(int n)
+{
+	if (n == 1 || n == 2)
+	{
+		return 1;
+	}
+	else
+	{
+		return fib(n - 1) + fib(n - 2);
+	}
+}
+
 int main() {
 	cout << "Hellow World!" << endl;
+	
+	int n; cin >> n;
+	cout << fib(n);
+
 	return 0;
 }
